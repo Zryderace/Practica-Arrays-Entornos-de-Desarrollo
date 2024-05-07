674d3225608032deca15fc9eceb3b9315cac7278
Zryderace <147004178+Zryderace@users.noreply.github.com>
2024-05-07 17:22:14 +0200

    Javadoc real

---
+++ PracticaArrays/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"tests","c":"AllTests","l":"AllTests()","u":"%3Cinit%3E()"},{"p":"Util","c":"Arrayss","l":"Arrayss()","u":"%3Cinit%3E()"},{"p":"Util","c":"Arrayss","l":"maximaNota(int[])"},{"p":"Util","c":"Arrayss","l":"medianaNotas(int[])"},{"p":"Util","c":"Arrayss","l":"mediaNotas(int[])"},{"p":"Util","c":"Arrayss","l":"minimaNota(int[])"}];updateSearchResults();
---
+++ PracticaArrays/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"tests","l":"AllTests"},{"p":"Util","l":"Arrayss"}];updateSearchResults();
